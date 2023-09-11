7207f34cea2e9c3ea2fac58bca064390d8bf5abb
vmisiko <victormisiko.vm@gmail.com>
2023-09-11 23:46:29 +0300

    Solved twosum algo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # data_structure_and_algo
+
+## How to run the tests 
+First transpile the code of each ts file to js using this command e.g
+ ```
+ tsc twoSum.ts
+ ```
+
+ This 
+This command uses the TypeScript compiler (tsc) to compile twoSum.ts into twoSum.js.
+
+## Now run the javascript File
+```
+node twoSum.js
+```
---
+++ twoSum.js
@@ -0,0 +1,22 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+var twoSum = function (nums, target) {
+    var hashMaps = {};
+    for (var i = 0; i < nums.length; i++) {
+        hashMaps[nums[i]] = i;
+    }
+    for (var i = 0; i < nums.length; i++) {
+        var diff = target - nums[i];
+        if (diff in hashMaps) {
+            return [i, hashMaps[diff]];
+        }
+    }
+};
+var nums = [2, 7, 11, 15];
+var target = 9;
+console.log(twoSum(nums, target));
